3322fa9b32e01c6b91025c32b92e7e43f8ff585a
haohaoalt <haohaoalt@163.com>
2023-04-02 22:18:06 +0800

    0402:python learning

---
+++ python_basic/basic_struct.py
@@ -0,0 +1,21 @@
+number = 2
+print(f"number={number}")
+str1 = 'hello'
+str2 = 'world'
+print(f"str1 + str2 ={str1+' '+str2}")
+demo = 'hello {0}! My name is {1}.'.format(str2,str1)
+print(demo)
+str_list = ['hello','world']
+print('\n'.join(str_list))
+for i in range(10):
+    print('hello :',i)
+
+print(0.1+0.2)
+name = "eric"
+print(name.lower())
+print(name.upper())
+print(name.title())
+numbers = list(range(3))
+print(numbers)
+Guests = ['zhanghao','李四','王五']
+print(f"{Guests[0].title()},诚邀您共进午餐！")
